74c63374dbd4c68765e9c9638bc73b3a314d2455
AntonioSantos8 <afsantos082008@gmail.com>
2025-03-13 13:39:32 -0300

    Move and camera
    add move and camera

---
+++ ZeldaSenacOi/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float moveSpeed;
+    Vector2 move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = move * moveSpeed;
+    }
+    void Move()
+    {
+        move.x = Input.GetAxisRaw("Horizontal");
+        move.y = Input.GetAxisRaw("Vertical");
+        move.Normalize();
+    }
+   
+}
