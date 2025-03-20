5a461cf46afc45422ce5ef568c81075606cb4c75
AntonioSantos8 <afsantos082008@gmail.com>
2025-03-20 13:21:53 -0300

    Dano dos inimigos agora funciona

--- ZeldaSenacOi/Assets/Scripts/Enemies/EnemyAttack.cs
+++ ZeldaSenacOi/Assets/Scripts/Enemies/EnemyAttack.cs
@@ -5,39 +5,35 @@ using UnityEngine;
 public class EnemyAttack : MonoBehaviour
 {
     PlayerStatus statusPlayer;
-    bool canAttack;
     Coroutine attackRoutine;
     public int damage;
     public int attackCooldown = 2;
+
     void Start()
     {
-        canAttack = true;
         statusPlayer = FindObjectOfType<PlayerStatus>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (canAttack && collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && attackRoutine == null)
         {
-            canAttack = true;
             attackRoutine = StartCoroutine(Attack());
-            StartCoroutine(Attack());
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && attackRoutine != null)
         {
             StopCoroutine(attackRoutine);
             attackRoutine = null;
-            canAttack = false;
         }
     }
 
     IEnumerator Attack()
     {
-        while (canAttack)
+        while (true)
         {
             statusPlayer.life -= damage;
             yield return new WaitForSeconds(2);
